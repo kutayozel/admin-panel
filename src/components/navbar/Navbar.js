f0228c6baff76dc7f1fda47444d9be330d3e2165
Kharza <zelkutay@gmail.com>
2022-05-01 11:45:04 +0300

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,50 @@
 import "./navbar.scss"
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined"
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined"
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined"
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined"
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined"
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon className="icon"/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon"/>
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon"/>
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon"/>
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon"/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon"/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon"/>
+          </div>
+          <div className="item">
+            <img src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?b=1&k=20&m=476085198&s=170667a&w=0&h=Ct4e1kIOdCOrEgvsQg4A1qeuQv944pPFORUQcaGw4oI=" 
+            alt="avatarimg" 
+            className="avatar"/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
