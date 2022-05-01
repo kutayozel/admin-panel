9df91657c1a71a2313fd41f51055f4f91ec3831f
Kharza <zelkutay@gmail.com>
2022-05-01 11:02:15 +0300

    sidebarstyle

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -21,57 +21,65 @@ const Sidebar = () => {
         <hr />
         <div className="center">
           <ul>
+            <p className="title">MAIN</p>
             <li>
-              <DashboardIcon/>
+              <DashboardIcon className = "icon"/>
               <span>Dashboard</span>
             </li>
+            <p className="title">LISTS</p>
             <li>
-              <PersonOutlineOutlinedIcon/>
+              <PersonOutlineOutlinedIcon className = "icon"/>
               <span>Users</span>
             </li>
             <li>
-            <ShoppingBagIcon/>
+            <ShoppingBagIcon className = "icon"/>
               <span>Products</span>
             </li>
             <li>
-            <CreditCardIcon/>
+            <CreditCardIcon className = "icon"/>
               <span>Orders</span>
             </li>
             <li>
-            <LocalShippingIcon/>
+            <LocalShippingIcon className = "icon"/>
               <span>Delivery</span>
             </li>
+            <p className="title">USEFUL</p>
             <li>
-            <QueryStatsIcon/>
+            <QueryStatsIcon className = "icon"/>
               <span>Stats</span>
             </li>
             <li>
-            <NotificationsNoneIcon/>
+            <NotificationsNoneIcon className = "icon"/>
               <span>Notifications</span>
             </li>
+            <p className="title">SERVICE</p>
             <li>
-            <WysiwygIcon/>
+            <WysiwygIcon className = "icon"/>
               <span>System Health</span>
             </li>
             <li>
-            <ManageSearchIcon/>
+            <ManageSearchIcon className = "icon"/>
               <span>Logs</span>
             </li>
             <li>
-            <SettingsOutlinedIcon/>
+            <SettingsOutlinedIcon className = "icon"/>
               <span>Settings</span>
             </li>
+            <p className="title">USER</p>
             <li>
-            <AccountCircleOutlinedIcon/>
+            <AccountCircleOutlinedIcon className = "icon"/>
               <span>Profile</span>
             </li>
             <li>
-            <ExitToAppOutlinedIcon/>
+            <ExitToAppOutlinedIcon className = "icon"/>
               <span>Logout</span>
             </li>
           </ul>
         </div>
-        <div className="bottom">color options</div>
+        <div className="bottom">
+          <div className="colorOption"></div>
+          <div className="colorOption"></div>
+        </div>
     </div>
   )
 }
