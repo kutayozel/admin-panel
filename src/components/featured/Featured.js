5c9ca71e78477490ca3924bd0cb52b6330b7803e
Kharza <zelkutay@gmail.com>
2022-05-02 03:31:58 +0300

    featured chart

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,55 @@
+import './featured.scss'
+import MoreVertIcon from "@mui/icons-material/MoreVert"
+import { CircularProgressBar } from "@tomik23/react-circular-progress-bar"
+import { KeyboardArrowDown } from '@mui/icons-material';
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp"
+
+function Featured() {
+  return (
+    <div className='featured'>
+        <div className="top">
+            <h1 className="title">Total Revenue</h1>
+            <MoreVertIcon fontSize='small'/>
+        </div>
+        <div className="bottom">
+            <div className="featuredChart">
+                <CircularProgressBar 
+                fontSize={"1.2rem"} 
+                size={100} 
+                percent={70}
+                stroke={5}
+                colorCircle="#e6e6e6"
+                />
+            </div>
+            <p className="title">Total sales made today</p>
+            <p className="amount">$420</p>
+            <p className="desc">Previous transaction processing</p>
+            <div className="summary">
+                <div className="item">
+                    <div className="itemTitle">Target</div>
+                    <div className="itemResult negative">
+                        <KeyboardArrowDown fontSize='small' />
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Week</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowUpIcon fontSize='small' />
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Month</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowUpIcon fontSize='small' />
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
