bb48216d6f4c737bb63219cf567dd86712772083
Kharza <zelkutay@gmail.com>
2022-05-05 05:19:44 +0300

    single info tab

--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import "./single.scss"
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Charts from "../../components/charts/Charts"
+import List from "../../components/table/Table"
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <Sidebar/>
+      <div className="singleContainer">
+        <Navbar/>
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?b=1&k=20&m=476085198&s=170667a&w=0&h=Ct4e1kIOdCOrEgvsQg4A1qeuQv944pPFORUQcaGw4oI=" alt="" className="itemImg" />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">465 48 465</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">dsakdlhksalkdshalı gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Charts/>
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List/>
+        </div>
+      </div>
+    </div>
   )
 }
 
